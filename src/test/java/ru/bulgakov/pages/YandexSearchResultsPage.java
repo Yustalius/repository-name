78a19c1d712110ec78ef64b8f2a46aa253452fc6
Владислав Юстус <V.Yustus@m-developer.ru>
2026-01-10 22:38:08 +0300

    refactor test to page object v1

---
+++ src/test/java/ru/bulgakov/pages/YandexSearchResultsPage.java
@@ -0,0 +1,22 @@
+package ru.bulgakov.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class YandexSearchResultsPage {
+  private final SelenideElement closeWindow = $(".DistributionButtonClose");
+
+  public YandexSearchResultsPage closeDefaultBrowserSelectWindow() {
+    closeWindow.click();
+
+    return this;
+  }
+
+  public WelcomePage openLink(String webSiteName) {
+    $(byText(webSiteName)).click();
+
+    return new WelcomePage();
+  }
+}
