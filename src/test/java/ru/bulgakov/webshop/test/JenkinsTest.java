74c3ffe4cf0345a5d8cc5b2a0423c447478f83cb
Владислав Юстус <V.Yustus@m-developer.ru>
2026-01-14 07:56:57 +0300

    jenkins check

---
+++ src/test/java/ru/bulgakov/webshop/test/JenkinsTest.java
@@ -0,0 +1,11 @@
+package ru.bulgakov.webshop.test;
+
+import org.junit.jupiter.api.Test;
+
+public class JenkinsTest {
+
+  @Test
+  void jenkinsCheck() {
+    System.out.println("Jenkins запускается!");
+  }
+}
